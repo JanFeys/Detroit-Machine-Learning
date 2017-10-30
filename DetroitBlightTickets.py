c7d9ce3f3010c208948088938f658f147c265564
Jan Feys <feys.jan@gmail.com>
2017-10-30 13:42:02 -0400

    Cleaned up python script

--- DetroitBlightTickets.py
+++ DetroitBlightTickets.py
@@ -14,21 +14,6 @@ def load_data():
     
     #drop entries for which compliance is "not responsible", i.e. those whose value is not 0 or 1 but NaN
     train_df.dropna(subset=['compliance'], inplace=True)
-        
-    ##some diagnostics:
-     #output all columns
-    print(list(train_df.columns.values))
-    # #print datatypes for the columns
-    #for col in train_df.columns:
-    #    print(train_df[col].dtype, col)
-     #count and print the occurrences of number in a column
-    #vc = train_df.disposition.value_counts(dropna=False)
-    #print(vc)
-     #print out columns and shape of dataframe for info
-    #print(list(train_df.columns.values))
-     #print shape of the data
-    #print(train_df.shape)
-    #print(test_df.shape)
     
     return train_df, test_df
 
@@ -55,33 +40,23 @@ def learn_data(train_df,test_df):
     X = (X - Xmean)*(1/Xstd)
     X_test = (test_df - Xmean)*(1/Xstd)
     
-    #['fine_amount', 'discount_amount', 'judgment_amount', 'compliance']
-    #vc = X_test.judgment_amount.value_counts(dropna=False)
-    #print(vc)
-
-    #train classifier
     clf = RandomForestClassifier(max_depth=25)
     
     scores = cross_val_score(clf, X, Y, cv=5, scoring='roc_auc')
     print(scores)
     print(np.mean(scores))
     
-    #predict
     clf.fit(X, Y)
     y_pred = clf.predict_proba(X_test)
     
     result_sr = pd.Series(data=y_pred[:,1], index=np.array(test_df.index.values.tolist()), dtype="float32")
     result_sr.index.name = 'ticket_id'
-    #print(result_sr)
-
     return result_sr
     
 def blight_model():
     train_df, test_df = load_data()
-    result_df = learn_data(train_df, test_df)
-    print('done!')
+    bm = learn_data(train_df, test_df)
     
-    bm = result_df
     res = 'Data type Test: '
     res += ['Failed: type(bm) should Series\n','Passed\n'][type(bm)==pd.Series]
     res += 'Data shape Test: '
@@ -94,10 +69,8 @@ def blight_model():
     res += ['Failed: type(bm.index) should be Int64Index\n','Passed\n'][type(bm.index)==pd.Int64Index]
     res += 'Index values type Test: '
     res += ['Failed: type(bm.index[0]) should be int64\n','Passed\n'][str(type(bm.index[0])).count("int64")>0]
-
     res += 'Output index shape test:'
     res += ['Failed, bm.index.shape should be (61001,)\n','Passed\n'][bm.index.shape==(61001,)]
-
     res += 'Output index test: '
     if bm.index.shape==(61001,):
         res +=['Failed\n','Passed\n'][all(pd.read_csv('data/test.csv',usecols=[0],index_col=0).sort_index().index.values==bm.sort_index().index.values)]
@@ -105,6 +78,6 @@ def blight_model():
         res+='Failed'
     print(res)
 
-    return result_df
+    return bm
 
 blight_model()
