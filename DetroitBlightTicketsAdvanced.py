0d6da29083c681c759a2b2e5dbc1862b74665fdc
Jan Feys <feys.jan@gmail.com>
2017-10-30 18:44:51 -0400

    Debugged advanced code

--- DetroitBlightTicketsAdvanced.py
+++ DetroitBlightTicketsAdvanced.py
@@ -38,6 +38,7 @@ def load_data():
     #set ticket_id as the index for both
     train_df.set_index('ticket_id', inplace=True)
     test_df.set_index('ticket_id', inplace=True)
+    
     return train_df, test_df
 
 def process_data(train_df,test_df):
@@ -47,7 +48,7 @@ def process_data(train_df,test_df):
     train_df.dropna(subset=['compliance'], inplace=True)
     
     #make compliance column integer
-    train_df.compliance = train_df.compliance.apply(np.int64)
+    train_df['compliance'] = train_df['compliance'].apply(np.int64)
     
     #there is one NaN entry in training set's fine_amount, we drop that entry
     train_df.dropna(subset=['fine_amount'], inplace=True)
@@ -84,7 +85,7 @@ def process_data(train_df,test_df):
     #print(test_df.isnull().any())
         
     #select the columns that are not float or int so we can make those features categorical
-    catcols = test_df.select_dtypes(exclude=['float', 'int']).columns
+    catcols = list(test_df.select_dtypes(exclude=['float', 'int']).columns)
     concat_df = pd.concat((train_df[catcols], test_df[catcols]), axis=0, verify_integrity=True)
 
     #clean up the violation codes: remove characters after space and bracket, remove codes without dash
@@ -103,7 +104,7 @@ def process_data(train_df,test_df):
 
     #make the features categorical
     for col in catcols:
-        dummies = pd.get_dummies(concat_df[col],sparse=True)
+        dummies = pd.get_dummies(concat_df[col]) #sparse=True can be ENABLED
         concat_df[dummies.columns] = dummies
         concat_df.drop(col, axis=1, inplace=True)
         train_df.drop(col, axis=1, inplace=True)
@@ -112,7 +113,6 @@ def process_data(train_df,test_df):
     #pour back into train and test set
     train_df[concat_df.columns] = concat_df.loc[train_df.index]
     test_df[concat_df.columns] = concat_df.loc[test_df.index]
-    
     return train_df, test_df
 
 def learn_data(train_df,test_df):
@@ -123,35 +123,66 @@ def learn_data(train_df,test_df):
 
     X = train_df[features]
     Y = np.array(train_df[response]).ravel()
+    
+    #normalize the numerical columns
+    numcols = list(X.select_dtypes(include=['float'],exclude=['int']).columns)
+    numcols = ['fine_amount', 'discount_amount', 'judgment_amount']
+    #print(X.shape)
+    for col in numcols:
+        #print(col)
+        X_col = X[col].values.astype(float)
+        test_df_col = test_df[col].values.astype(float)
+        #print(X_col,test_df_col)
+        col_mean = X_col.mean()
+        col_std = test_df_col.std()
+        #print(col_mean,col_std)
+        X[col+'_norm'] = (X_col-col_mean)*(1/col_std)
+        test_df[col+'_norm'] = (test_df_col-col_mean)*(1/col_std)
 
-    print(list(train_df.columns.values))
-    
-    print('$$$$$$$$$$$$$$$')
-    # Normalize
-    X_norm=(X-X.min())/(X.max()-X.min())
-        
-    # Train classifier
-    #clf = RandomForestClassifier(max_depth=25)
-    #clf.fit(X, Y)
-    
-    # Predict
-    #y_pred = np.array(clf.predict(Xtest))
-    #y_pred = y_pred - y_pred.min()
-    #y_pred = y_pred / y_pred.max()
+    for col in numcols:
+        X.drop(col, axis=1, inplace=True)
+        test_df.drop(col, axis=1, inplace=True)
 
-    # Save
-    #result_df = {"ticket_id":test_ticket_id, "compliance":y_pred}
-    #result_df = pd.DataFrame(result_df, columns=["ticket_id", "compliance"])
-      
-    #print('$$$$$$')
-    #print(result_df)
+    clf = RandomForestClassifier(max_depth=15)
+    scores = cross_val_score(clf, X, Y, cv=5, scoring='roc_auc')
+    print(scores)
+
+    plot_validation_curve(X,Y)
+    quit()
     
+    clf.fit(X, Y)
+    y_pred = clf.predict_proba(X_test)
+    
+    result_sr = pd.Series(data=y_pred[:,1], index=np.array(test_df.index.values.tolist()), dtype="float32")
+    result_sr.index.name = 'ticket_id'
+    return result_sr
+
+def plot_validation_curve(X,Y):
+    param_range = np.linspace(5, 45, 9)
+    print('range of parameter max_depth:',param_range)
+    train_scores, test_scores = validation_curve(RandomForestClassifier(), X, Y, param_name="max_depth",
+                                                 param_range=param_range,cv=5, scoring="roc_auc", n_jobs=1)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    plt.title("Validation Curve with RandomForestClassifier")
+    plt.xlabel("max_depth")
+    plt.ylabel("roc_auc")
+    plt.ylim(0.5, 1)
+    lw = 2
+    plt.plot(param_range, train_scores_mean, label="Training score",color="darkorange", lw=lw)
+    plt.fill_between(param_range, train_scores_mean - train_scores_std,train_scores_mean + train_scores_std, alpha=0.2,color="darkorange", lw=lw)
+    plt.plot(param_range, test_scores_mean, label="Cross-validation score",color="navy", lw=lw)
+    plt.fill_between(param_range, test_scores_mean - test_scores_std,test_scores_mean + test_scores_std, alpha=0.2,color="navy", lw=lw)
+    plt.legend(loc="best")
+    plt.show()
+
 def blight_model():
     train_df, test_df = load_data()
     train_df, test_df = process_data(train_df,test_df)
-    #learn_data(train_df, test_df)
-    print('done!')
-    print(dir())
+    learn_data(train_df, test_df)
     return None
 
 blight_model()
