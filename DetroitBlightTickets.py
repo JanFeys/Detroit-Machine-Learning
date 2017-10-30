42e16f5c0f13c9cc69453e35b5fdfdcd36999102
Jan Feys <feys.jan@gmail.com>
2017-10-30 16:05:22 -0400

    Implemented validation curve

--- DetroitBlightTickets.py
+++ DetroitBlightTickets.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
+from sklearn.model_selection import validation_curve
 
 def load_data():
     print('loading data')
@@ -42,7 +44,32 @@ def learn_data(train_df,test_df):
     
     result_sr = pd.Series(data=y_pred[:,1], index=np.array(test_df.index.values.tolist()), dtype="float32")
     result_sr.index.name = 'ticket_id'
+    
+    plot_validation_curve(X,Y)
+    
     return result_sr
+
+def plot_validation_curve(X,Y):
+    param_range = np.linspace(10, 40, 4)
+    
+    train_scores, test_scores = validation_curve(RandomForestClassifier(), X, Y, param_name="max_depth", param_range=param_range,
+    cv=5, scoring="roc_auc", n_jobs=1)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    plt.title("Validation Curve with RandomForestClassifier")
+    plt.xlabel("max_depth")
+    plt.ylabel("roc_auc")
+    plt.ylim(0.7, 0.8)
+    lw = 2
+    plt.plot(param_range, train_scores_mean, label="Training score",color="darkorange", lw=lw)
+    plt.fill_between(param_range, train_scores_mean - train_scores_std,train_scores_mean + train_scores_std, alpha=0.2,color="darkorange", lw=lw)
+    plt.plot(param_range, test_scores_mean, label="Cross-validation score",color="navy", lw=lw)
+    plt.fill_between(param_range, test_scores_mean - test_scores_std,test_scores_mean + test_scores_std, alpha=0.2,color="navy", lw=lw)
+    plt.legend(loc="best")
+    plt.show()
     
 def blight_model():
     train_df, test_df = load_data()
